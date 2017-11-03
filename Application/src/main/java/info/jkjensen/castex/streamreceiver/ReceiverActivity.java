a8ce9fd1707ab0d89246059fc4c98ce2c3a4e952
Jk Jensen <azjkjensen@gmail.com>
2017-11-03 14:35:32 -0700

    Worked a bit on improving reliability.

--- Application/src/main/java/info/jkjensen/castex/streamreceiver/ReceiverActivity.java
+++ Application/src/main/java/info/jkjensen/castex/streamreceiver/ReceiverActivity.java
@@ -108,6 +108,7 @@ public class ReceiverActivity extends Activity implements TextureView.SurfaceTex
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receiver);
+        Log.d("ReturnToActivityTest", "onCreate Called!");
 
         // Configure receiver preferences
         SharedPreferences sharedPreferences = getSharedPreferences("appConfig", Context.MODE_PRIVATE);
@@ -129,7 +130,13 @@ public class ReceiverActivity extends Activity implements TextureView.SurfaceTex
     }
 
     @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
     protected void onStop() {
+        Log.e("ReceiverActivity", "onStop called");
         super.onStop();
         if(mTimeAnimator != null && mTimeAnimator.isRunning()) {
             mTimeAnimator.end();
@@ -142,12 +149,33 @@ public class ReceiverActivity extends Activity implements TextureView.SurfaceTex
             packetReceiverTask.cancel(true);
         }
         if(mediaCodec != null) {
+            mediaCodec.stop();
             mediaCodec.release();
         }
+        if(clientSocket != null && !clientSocket.isClosed()){
+            clientSocket.close();
+        }
+        if(tSock != null && !tSock.isClosed()){
+            try {
+                tSock.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(fileOutputStream != null){
+            try {
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            fileOutputStream = null;
+        }
+        mPlaybackView = null;
     }
 
     @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        Log.d("ReturnToActivityTest", "Surface Texture Available!");
         // Set up the UDP socket for receiving data.
         try {
 //            if(clientSocket == null || !clientSocket.isConnected()) {
@@ -238,46 +266,73 @@ public class ReceiverActivity extends Activity implements TextureView.SurfaceTex
         return true;
     }
 
-    public void startPlayback() {
-        startUs = System.nanoTime() / 1000;
+    private class DecodeFrameTask extends AsyncTask<String, String, String> {
 
-        if(debugEnabled && DEBUG_WRITE_TO_FILE) {
-            SimpleDateFormat s = new SimpleDateFormat("ddMMyyyyhhmmss");
-            File fileOut = new File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "testReceiverOutput" + s.format(new Date()) + ".txt");
-            try {
-                Log.d("Main", "Writing to " + fileOut.toString());
-                fileOutputStream = new FileOutputStream(fileOut, true);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.d("ReturnToActivityTest", "Decoding in background");
+            return startPlayback();
         }
 
-        MediaFormat format = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC,
-                streamWidth, streamHeight);
-        format.setInteger(MediaFormat.KEY_FRAME_RATE, STREAMING_FRAME_RATE);
-        format.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 0);
-        NALBuffer sps;
-        // Get NAL Units until an SPS unit is found.
-        while(true){
-            while(peekQueue() == null);
-            sps = nalParser.getNext(pollQueue());
-            Log.d("Main", "searching for an SPS frame...");
-            if((sps.buffer.get(4) & 0x1f) != 0x07){
-                Log.d("Main", "Not an SPS Frame");
-                continue;
-            }
-            Log.d("Main", "Found an SPS Frame");
+        @Override
+        protected void onCancelled(String s) {
+            super.onCancelled(s);
+            Log.d("ReturnToActivityTest", "Decoding task cancelled");
+        }
 
-            while(peekQueue() == null);
-            NALBuffer pps = nalParser.getNext(pollQueue());
-            if((pps.buffer.get(4) & 0x1f) != 0x08){
-                Log.d("Main", "Not a PPS Frame");
-                continue;
+        @Override
+        protected void onPostExecute(String result){
+            Log.d("ReturnToActivityTest", "Decoding task stopped");
+            try{
+                mediaCodec.stop();
+                mediaCodec.release();
+            } catch (Exception e){
+                e.printStackTrace();
             }
-            Log.d("Main", "Found a PPS Frame");
+//            nalParser.release();
+        }
 
-            format.setByteBuffer("csd-0", sps.buffer);
-            format.setByteBuffer("csd-1", pps.buffer);
+        private String startPlayback() {
+            startUs = System.nanoTime() / 1000;
+
+            if(debugEnabled && DEBUG_WRITE_TO_FILE) {
+                SimpleDateFormat s = new SimpleDateFormat("ddMMyyyyhhmmss");
+                File fileOut = new File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "testReceiverOutput" + s.format(new Date()) + ".txt");
+                try {
+                    Log.d("Main", "Writing to " + fileOut.toString());
+                    fileOutputStream = new FileOutputStream(fileOut, true);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            MediaFormat format = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC,
+                    streamWidth, streamHeight);
+            format.setInteger(MediaFormat.KEY_FRAME_RATE, STREAMING_FRAME_RATE);
+            format.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 0);
+            NALBuffer sps;
+            // Get NAL Units until an SPS unit is found.
+            while(true){
+                if(isCancelled()) return "";
+                while(peekQueue() == null); // TODO: Fix this to time out.
+                sps = nalParser.getNext(pollQueue());
+                Log.d("Main", "searching for an SPS frame...");
+                if((sps.buffer.get(4) & 0x1f) != 0x07){
+                    Log.d("Main", "Not an SPS Frame");
+                    continue;
+                }
+                Log.d("Main", "Found an SPS Frame");
+
+                while(peekQueue() == null); // TODO: Fix this to time out.
+                NALBuffer pps = nalParser.getNext(pollQueue());
+                if((pps.buffer.get(4) & 0x1f) != 0x08){
+                    Log.d("Main", "Not a PPS Frame");
+                    continue;
+                }
+                Log.d("Main", "Found a PPS Frame");
+
+                format.setByteBuffer("csd-0", sps.buffer);
+                format.setByteBuffer("csd-1", pps.buffer);
 //            try {
 //                if(debugEnabled && DEBUG_WRITE_TO_FILE){
 //                    fileOutputStream.write(sps.buffer.array());
@@ -287,54 +342,54 @@ public class ReceiverActivity extends Activity implements TextureView.SurfaceTex
 //                e.printStackTrace();
 //            }
 
-            try {
-                mediaCodec = MediaCodec.createDecoderByType(MediaFormat.MIMETYPE_VIDEO_AVC);
-                mediaCodec.configure(format, new Surface(mPlaybackView.getSurfaceTexture()), null, 0);
-                mediaCodec.start();
-                break;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        Log.e("Main", "------------------- CONFIG FRAMES ABOVE THIS POINT ---------------------");
-        long totaliFrameBytes = 0;
-        long totaliFrames = 0;
-
-        Boolean isIFrame = false;
-        while(true) {
-
-            while(peekQueue() == null);
-            NALBuffer nb = nalParser.getNext(pollQueue());
-
-            int type = nb.buffer.get(4);
-            if(debugEnabled && DEBUG_WRITE_TO_FILE) try {
-                fileOutputStream.write((nb.buffer.limit() + "\n").getBytes());
-            } catch (IOException e) {
-                e.printStackTrace();
+                try {
+                    mediaCodec = MediaCodec.createDecoderByType(MediaFormat.MIMETYPE_VIDEO_AVC);
+                    mediaCodec.configure(format, new Surface(mPlaybackView.getSurfaceTexture()), null, 0);
+                    mediaCodec.start();
+                    break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
-            if((type & 0x1f) == 0x05){
-                totaliFrameBytes += nb.buffer.limit();
-                totaliFrames++;
-                isIFrame = true;
-                Log.d("iFrame", "Current totals - iframes: " + totaliFrames + ", bytes: " + totaliFrameBytes);
-            }
-            nb.buffer.rewind();
+            Log.e("Main", "------------------- CONFIG FRAMES ABOVE THIS POINT ---------------------");
+            long totaliFrameBytes = 0;
+            long totaliFrames = 0;
 
-            if (nb == null) {
-                Log.e("Main", "Couldn't get NAL");
-                return;
-            }
+            Boolean isIFrame = false;
+            while(true) {
+                if(isCancelled()) return "";
+                while(peekQueue() == null); // TODO: Fix this to time out.
+                NALBuffer nb = nalParser.getNext(pollQueue());
 
-            int inputIndex;
-            while ((inputIndex = mediaCodec.dequeueInputBuffer(-1)) < 0) {
-                Log.d("Main", "Input index: " + inputIndex);
-            }
+                int type = nb.buffer.get(4);
+                if(debugEnabled && DEBUG_WRITE_TO_FILE) try {
+                    fileOutputStream.write((nb.buffer.limit() + "\n").getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if((type & 0x1f) == 0x05){
+                    totaliFrameBytes += nb.buffer.limit();
+                    totaliFrames++;
+                    isIFrame = true;
+                    Log.d("iFrame", "Current totals - iframes: " + totaliFrames + ", bytes: " + totaliFrameBytes);
+                }
+                nb.buffer.rewind();
+
+                if (nb == null) {
+                    Log.e("Main", "Couldn't get NAL");
+                    return "";
+                }
+
+                int inputIndex;
+                while ((inputIndex = mediaCodec.dequeueInputBuffer(-1)) < 0) {
+                    Log.d("Main", "Input index: " + inputIndex);
+                }
 //            Log.d("Main", "Final Input index: " + inputIndex);
 
-            ByteBuffer codecBuffer = mediaCodec.getInputBuffer(inputIndex);
-            codecBuffer.put(nb.buffer);
+                ByteBuffer codecBuffer = mediaCodec.getInputBuffer(inputIndex);
+                codecBuffer.put(nb.buffer);
 
 //            try {
 //                if(debugEnabled && DEBUG_WRITE_TO_FILE)fileOutputStream.write(nb.buffer.array());
@@ -342,46 +397,27 @@ public class ReceiverActivity extends Activity implements TextureView.SurfaceTex
 //                e.printStackTrace();
 //            }
 
-            long presentationTimeMS = (System.nanoTime()/1000) - startUs; // Don't think this is necessary.
+                long presentationTimeMS = (System.nanoTime()/1000) - startUs; // Don't think this is necessary.
 
-            int flags = 0;
-            // If this is the final NAL Unit we can signify the end of the stream.
+                int flags = 0;
+                // If this is the final NAL Unit we can signify the end of the stream.
 //            if(nb.lastNAL){
 //                flags = MediaCodec.BUFFER_FLAG_END_OF_STREAM;
 //            }
-            if((type & 0x1f) == 0x07 || (type & 0x1f) == 0x08){
-                Log.e("SPSPPS", "Found new SPS, PPS");
-                mediaCodec.queueInputBuffer(inputIndex, 0, nb.size, /*presentationTimeMS*/0, MediaCodec.BUFFER_FLAG_CODEC_CONFIG);
-            } else {
-                mediaCodec.queueInputBuffer(inputIndex, 0, nb.size, /*presentationTimeMS*/0, isIFrame ? MediaCodec.BUFFER_FLAG_KEY_FRAME : flags);
+                if((type & 0x1f) == 0x07 || (type & 0x1f) == 0x08){
+                    Log.e("SPSPPS", "Found new SPS, PPS");
+                    mediaCodec.queueInputBuffer(inputIndex, 0, nb.size, /*presentationTimeMS*/0, MediaCodec.BUFFER_FLAG_CODEC_CONFIG);
+                } else {
+                    mediaCodec.queueInputBuffer(inputIndex, 0, nb.size, /*presentationTimeMS*/0, isIFrame ? MediaCodec.BUFFER_FLAG_KEY_FRAME : flags);
+                }
+
+                MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
+                int outputIndex = mediaCodec.dequeueOutputBuffer(info, 0);
+                if (outputIndex >= 0) {
+                    mediaCodec.releaseOutputBuffer(outputIndex, true);
+                }
+                isIFrame = false;
             }
-
-            MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
-            int outputIndex = mediaCodec.dequeueOutputBuffer(info, 0);
-            if (outputIndex >= 0) {
-                mediaCodec.releaseOutputBuffer(outputIndex, true);
-            }
-            isIFrame = false;
-        }
-    }
-
-    private class DecodeFrameTask extends AsyncTask<String, String, String> {
-
-        @Override
-        protected String doInBackground(String... strings) {
-            startPlayback();
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result){
-            try{
-                mediaCodec.stop();
-                mediaCodec.release();
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-//            nalParser.release();
         }
     }
 
@@ -402,6 +438,7 @@ public class ReceiverActivity extends Activity implements TextureView.SurfaceTex
         protected String doInBackground(String... strings) {
             // Continuously receive packets and put them on a priorityqueue
             while(true){
+                if(isCancelled()) return "";
                 try {
                     byte[] buff = new byte[100535];
                     dPacket = new DatagramPacket(buff, 100535);
@@ -451,6 +488,7 @@ public class ReceiverActivity extends Activity implements TextureView.SurfaceTex
                     dPacket.setData(buf.array(), buf.arrayOffset(), buf.limit());
                     addToQueue(ByteBuffer.wrap(dPacket.getData(), dPacket.getOffset(), dPacket.getLength()).duplicate());
                 } catch (IOException e) {
+                    if(isCancelled()) return "";
                     e.printStackTrace();
                 }
             }
